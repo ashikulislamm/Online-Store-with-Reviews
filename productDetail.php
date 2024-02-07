1880e4a9c720c108530c3711c4b2645a875a68e9
Ashikul Islam <ashikulislam780@gmail.com>
2024-02-07 11:18:35 +0600

    Review Bug Fixed

--- productDetail.php
+++ productDetail.php
@@ -156,6 +156,149 @@
             }
         }
         ?>
+        <!-------Review Section---------->
+        <div class="title">Reviews</div>
+        <div class="rcard">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-4 text-center">
+                        <h1 class="text-warning mt-4 mb-4">
+                            <?php
+                            $sql = "SELECT AVG(rating) as average_rating, COUNT(*) as total_review";
+                            $sql .= " FROM review WHERE product_id = $productId";
+                            $result = mysqli_query($con, $sql);
+                            $row = $result->fetch_assoc();
+                            echo '<b><span id="average_rating">' . number_format($row['average_rating'], 1) . '</span> / 5</b>';
+                            ?>
+
+                        </h1>
+                        <!---<div class="mb-3">
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                        </div>--->
+                        <?php
+                        echo '<h3><span id="total_review">' . $row['total_review'] . '</span> Review</h3>';
+                        ?>
+
+                    </div>
+                    <div class="col-sm-4">
+                        <p>
+                        <div class="progress-label-left"><b>5</b> <i class="fas fa-star text-warning"></i></div>
+                        <?php
+                        //Calculate the total  star review
+                        $totalReviewsql = "SELECT COUNT(*) as total_review FROM review WHERE product_id = $productId";
+                        $totalReviewresult = mysqli_query($con, $totalReviewsql);
+                        $totalReview = $totalReviewresult->fetch_assoc();
+                        $total_review = $totalReview['total_review'];
+
+                        $sql = "SELECT COUNT(*) as total_five_star_review FROM review WHERE product_id = $productId AND rating = 5";
+                        $result = mysqli_query($con, $sql);
+                        $row = $result->fetch_assoc();
+                        $total_five_star_review = $row['total_five_star_review'];
+                        echo '<div class="progress-label-right">(<span id="total_five_star_review">' . $row['total_five_star_review'] . '</span>)</div>';
+                        ?>
+                        <div class="progress">
+                            <?php
+                            $percentage = 0;
+                            if ($total_review > 0) {
+                                $percentage = ($total_five_star_review / $total_review) * 100;
+                            }
+                            echo '<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" id="five_star_progress" style="width: ' . $percentage . '%;"></div>';
+                            ?>
+                        </div>
+                        </p>
+                        <p>
+                        <div class="progress-label-left"><b>4</b> <i class="fas fa-star text-warning"></i></div>
+                        <?php
+                        $sql = "SELECT COUNT(*) as total_four_star_review FROM review WHERE product_id = $productId AND rating = 4";
+                        $result = mysqli_query($con, $sql);
+                        $row = $result->fetch_assoc();
+                        $total_four_star_review = $row['total_four_star_review'];
+                        echo '<div class="progress-label-right">(<span id="total_five_star_review">' . $row['total_four_star_review'] . '</span>)</div>';
+                        ?>
+                        <div class="progress">
+                            <?php
+                            $percentage = 0;
+                            if($total_review > 0){
+                                $percentage = ($total_four_star_review / $total_review) * 100;
+                            }
+                            echo '<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" id="five_star_progress" style="width: ' . $percentage . '%;"></div>';
+                            ?>
+                        </div>
+                        </p>
+                        <p>
+                        <div class="progress-label-left"><b>3</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <?php
+                        $sql = "SELECT COUNT(*) as total_three_star_review FROM review WHERE product_id = $productId AND rating = 3";
+                        $result = mysqli_query($con, $sql);
+                        $row = $result->fetch_assoc();
+                        $total_three_star_review = $row['total_three_star_review'];
+                        echo '<div class="progress-label-right">(<span id="total_five_star_review">' . $row['total_three_star_review'] . '</span>)</div>';
+                        ?>
+                        <div class="progress">
+                            <?php
+                            $percentage = 0;
+                            if($total_review > 0)
+                            {
+                                $percentage = ($total_three_star_review / $total_review) * 100;
+                            }
+                            echo '<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" id="five_star_progress" style="width: ' . $percentage . '%;"></div>';
+                            ?>
+                        </div>
+                        </p>
+                        <p>
+                        <div class="progress-label-left"><b>2</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <?php
+                        $sql = "SELECT COUNT(*) as total_two_star_review FROM review WHERE product_id = $productId AND rating = 2";
+                        $result = mysqli_query($con, $sql);
+                        $row = $result->fetch_assoc();
+                        $total_two_star_review = $row['total_two_star_review'];
+                        echo '<div class="progress-label-right">(<span id="total_five_star_review">' . $row['total_two_star_review'] . '</span>)</div>';
+                        ?>
+                        <div class="progress">
+                            <?php
+                            $percentage = 0;
+                            if($total_review > 0)
+                            {
+                                $percentage = ($total_two_star_review / $total_review) * 100;
+                            }
+                            echo '<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" id="five_star_progress" style="width: ' . $percentage . '%;"></div>';
+                            ?>
+                        </div>
+                        </p>
+                        <p>
+                        <div class="progress-label-left"><b>1</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <?php
+                        $sql = "SELECT COUNT(*) as total_one_star_review FROM review WHERE product_id = $productId AND rating = 1";
+                        $result = mysqli_query($con, $sql);
+                        $row = $result->fetch_assoc();
+                        $total_one_star_review = $row['total_one_star_review'];
+                        echo '<div class="progress-label-right">(<span id="total_five_star_review">' . $row['total_one_star_review'] . '</span>)</div>';
+                        ?>
+                        <div class="progress">
+                            <?php
+                            $percentage = 0;
+                            if($total_review > 0){
+                                $percentage = ($total_one_star_review / $total_review) * 100;
+                            }
+                            echo '<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" id="five_star_progress" style="width: ' . $percentage . '%;"></div>';
+                            ?>
+                        </div>
+                        </p>
+                    </div>
+                    <div class="col-sm-4 text-center">
+                        <h3 class="mt-4 mb-3">Write Review Here</h3>
+                        <button type="button" name="add_review" id="add_review" class="btn btn-primary review__btn">Review</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <?php
@@ -205,110 +348,37 @@
     }
     ?>
 
-    <!-------Review Section---------->
-    <div class="title">Reviews</div>
-    <div class="rcard">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-sm-4 text-center">
-                    <h1 class="text-warning mt-4 mb-4">
-                        <?php
-                        $sql = "SELECT AVG(rating) as average_rating, COUNT(*) as total_review";
-                        $sql .= " FROM review WHERE product_id = $productId";
-                        $result = mysqli_query($con, $sql);
-                        $row = $result->fetch_assoc();
-                        echo '<b><span id="average_rating">' . number_format($row['average_rating'], 1) . '</span> / 5</b>';
-                        ?>
 
-                    </h1>
-                    <div class="mb-3">
-                        <i class="fas fa-star star-light mr-1 main_star"></i>
-                        <i class="fas fa-star star-light mr-1 main_star"></i>
-                        <i class="fas fa-star star-light mr-1 main_star"></i>
-                        <i class="fas fa-star star-light mr-1 main_star"></i>
-                        <i class="fas fa-star star-light mr-1 main_star"></i>
-                    </div>
-                    <?php
-                    echo '<h3><span id="total_review">' . $row['total_review'] . '</span> Review</h3>';
-                    ?>
-
-                </div>
-                <div class="col-sm-4">
-                    <p>
-                    <div class="progress-label-left"><b>5</b> <i class="fas fa-star text-warning"></i></div>
-
-                    <div class="progress-label-right">(<span id="total_five_star_review">0</span>)</div>
-                    <div class="progress">
-                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="five_star_progress"></div>
-                    </div>
-                    </p>
-                    <p>
-                    <div class="progress-label-left"><b>4</b> <i class="fas fa-star text-warning"></i></div>
-
-                    <div class="progress-label-right">(<span id="total_four_star_review">0</span>)</div>
-                    <div class="progress">
-                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="four_star_progress"></div>
-                    </div>
-                    </p>
-                    <p>
-                    <div class="progress-label-left"><b>3</b> <i class="fas fa-star text-warning"></i></div>
-
-                    <div class="progress-label-right">(<span id="total_three_star_review">0</span>)</div>
-                    <div class="progress">
-                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="three_star_progress"></div>
-                    </div>
-                    </p>
-                    <p>
-                    <div class="progress-label-left"><b>2</b> <i class="fas fa-star text-warning"></i></div>
-
-                    <div class="progress-label-right">(<span id="total_two_star_review">0</span>)</div>
-                    <div class="progress">
-                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="two_star_progress"></div>
-                    </div>
-                    </p>
-                    <p>
-                    <div class="progress-label-left"><b>1</b> <i class="fas fa-star text-warning"></i></div>
-
-                    <div class="progress-label-right">(<span id="total_one_star_review">0</span>)</div>
-                    <div class="progress">
-                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="one_star_progress"></div>
-                    </div>
-                    </p>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <h3 class="mt-4 mb-3">Write Review Here</h3>
-                    <button type="button" name="add_review" id="add_review" class="btn btn-primary review__btn">Review</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-------Review Content---------->
-    <div class="mt-5" id="review_content">
-        <?php
-        $sql = "SELECT * FROM review WHERE product_id = $productId ORDER BY review_id DESC";
-        $result = mysqli_query($con, $sql);
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '
+    <div class="container">
+        <div class="contentCard" id="review_content">
+            <?php
+            $sql = "SELECT * FROM review WHERE product_id = $productId ORDER BY review_id DESC";
+            $result = mysqli_query($con, $sql);
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '
                     <div class="rcard">
                         <div class="card-body">
                             <h4 class="card-title">' . $row['user_name'] . '</h4>
                             <h5 class="card-subtitle mb-2 text-muted">';
-                for ($i = 1; $i <= 5; $i++) {
-                    if ($i <= $row['rating']) {
-                        echo '<i class="fas fa-star text-warning"></i>';
-                    } else {
-                        echo '<i class="far fa-star text-warning"></i>';
+                    for ($i = 1; $i <= 5; $i++) {
+                        if ($i <= $row['rating']) {
+                            echo '<i class="fas fa-star text-warning"></i>';
+                        } else {
+                            echo '<i class="far fa-star text-warning"></i>';
+                        }
                     }
-                }
-                echo '
+                    echo '
                             </h5>
                             <p class="card-text">' . $row['user_review'] . '</p>
                             <p class="card-text"><small class="text-muted">' .  $row["date_time"] . '</small></p>
+                        </div>
                         </div>';
+                }
             }
-        }
-        ?>
+            ?>
+        </div>
     </div>
     <!-------Review Modal---------->
     <div id="review_modal" class="modal" tabindex="-1" role="dialog">
@@ -355,10 +425,81 @@
             </div>
         </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            var rating_data = 0;
+
+            $("#add_review").click(function() {
+                $("#review_modal").modal("show");
+            });
+
+            $(document).on("mouseenter", ".submit_star", function() {
+                var rating = $(this).data("rating");
+
+                reset_background();
+
+                for (var count = 1; count <= rating; count++) {
+                    $("#submit_star_" + count).addClass("text-warning");
+                }
+            });
+
+            function reset_background() {
+                for (var count = 1; count <= 5; count++) {
+                    $("#submit_star_" + count).addClass("star-light");
+
+                    $("#submit_star_" + count).removeClass("text-warning");
+                }
+            }
+
+            $(document).on("mouseleave", ".submit_star", function() {
+                reset_background();
+
+                for (var count = 1; count <= rating_data; count++) {
+                    $("#submit_star_" + count).removeClass("star-light");
+
+                    $("#submit_star_" + count).addClass("text-warning");
+                }
+            });
+
+            $(document).on("click", ".submit_star", function() {
+                rating_data = $(this).data("rating");
+            });
+
+            $("#save_review").click(function() {
+                var user_name = $("#user_name").val();
+                var productId = $("#productId").val();
+                var user_review = $("#user_review").val();
+
+                if (user_name == "" || user_review == "") {
+                    alert("Please Fill Both Field");
+                    return false;
+                } else {
+                    $.ajax({
+                        url: "submit_review.php",
+                        method: "POST",
+                        data: {
+                            productId: productId,
+                            user_review: user_review,
+                            user_name: user_name,
+                            rating_data: rating_data,
+                        },
+                        success: function(data) {
+                            $("#review_modal").modal("hide");
+
+                            //load_rating_data();
+
+                            alert(data);
+                        },
+                    });
+                }
+            });
+        });
+    </script>
 
     <!-------Footer Section---------->
     <?php include 'footer.php'; ?>
     <!-------------------Custom JS------------------->
+
     <script src="/asset/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
